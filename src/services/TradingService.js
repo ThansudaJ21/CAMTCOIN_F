6af642a0bb95f93864a1661c164e1c8cf5dcbad8
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2022-02-08 22:36:27 +0700

    change tradingservice in methof buyCoins

--- src/services/TradingService.js
+++ src/services/TradingService.js
@@ -1,17 +1,16 @@
 import apiClient from '@/services/AxiosClient.js'
 
 export default {
-    getUser(id) {
-        return apiClient.get('/user/' + id)
-    },
-    getCoinGenerate() {
-        return apiClient.get('/coin/generate')
-    },
-    sellCoin(id, coin) {
-        return apiClient.post('/user/' + id + '/sell ', coin)
-    },
-    buyCoin(coin) {
-        return apiClient.put('/user/{id}/buy', coin)
-    },
-}
+  getUser(id) {
+    return apiClient.get('/user/' + id)
+  },
+  getCoinGenerate() {
+    return apiClient.get('/coin/generate')
+  },
+  sellCoin(id, coin) {
+    return apiClient.post('/user/' + id + '/sell ', coin)
+  },
+  buyCoin(id, amount) {
+    return apiClient.post('/user/' + id + '/buy', amount)
+  },
 }
